690acab3a7cd6f37150c4c4241894a1404e9770b
MITESH PATEL <mitforu@gmail.com>
2018-01-01 12:50:38 -0600

    fixing route

--- routes/dynamodb.js
+++ routes/dynamodb.js
@@ -2,6 +2,10 @@ var AWS = require("aws-sdk");
 var express = require('express');
 var router = express.Router();
 
+router.get('/test', function(req, res, next) {
+    res.send("this is working")
+})
+
 router.get('/create', function(req, res, next) {
     AWS.config.region = 'us-east-1';
 
@@ -26,8 +30,10 @@ router.get('/create', function(req, res, next) {
     dynamodb.createTable(params, function(err, data) {
         if (err) {
             console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+            res.send(JSON.stringify(err, null, 2));
         } else {
             console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+            res.send(JSON.stringify(data, null, 2));
         }
     });
 });
